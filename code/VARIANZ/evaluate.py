e409a0211765151439a84626763635daa55d45de
Sebastiano Barbieri <sbar288@UoA.auckland.ac.nz>
2020-10-26 02:39:03 +1300

    eval on folds and swap

--- code/VARIANZ/evaluate.py
+++ code/VARIANZ/evaluate.py
@@ -32,53 +32,74 @@ def main():
     event = data['event']
     
     df = pd.DataFrame({'TIME': data['time'], 'EVENT': data['event']})
-    df_cox = df.copy()
-    df_cml = df.copy()
 
-    # load log partial hazards
-    df_cox['LPH'] = feather.read_dataframe(hp.results_dir + 'df_cox_' + hp.gender + '.feather')['LPH']
-    df_cml['LPH'] = feather.read_dataframe(hp.results_dir + 'df_cml_' + hp.gender + '.feather')['LPH']
-    
-    ################################################################################################
-    
     # evaluation vectors
-    d_index_vec_cox = np.zeros(hp.num_folds)
-    r2_vec_cox = np.zeros(hp.num_folds)
-    concordance_vec_cox = np.zeros(hp.num_folds)
-    ibs_vec_cox = np.zeros(hp.num_folds)
-    auc_vec_cox = np.zeros(hp.num_folds)
+    d_index_vec_cox = np.zeros((hp.num_folds, 2))
+    r2_vec_cox = np.zeros((hp.num_folds, 2))
+    concordance_vec_cox = np.zeros((hp.num_folds, 2))
+    ibs_vec_cox = np.zeros((hp.num_folds, 2))
+    auc_vec_cox = np.zeros((hp.num_folds, 2))
 
-    d_index_vec_cml = np.zeros(hp.num_folds)
-    r2_vec_cml = np.zeros(hp.num_folds)
-    concordance_vec_cml = np.zeros(hp.num_folds)
-    ibs_vec_cml = np.zeros(hp.num_folds)
-    auc_vec_cml = np.zeros(hp.num_folds)
+    d_index_vec_cml = np.zeros((hp.num_folds, 2))
+    r2_vec_cml = np.zeros((hp.num_folds, 2))
+    concordance_vec_cml = np.zeros((hp.num_folds, 2))
+    ibs_vec_cml = np.zeros((hp.num_folds, 2))
+    auc_vec_cml = np.zeros((hp.num_folds, 2))
     
-    # baseline survival
-    es_cox = EvalSurv(df_cox.copy())
-    es_cml = EvalSurv(df_cml.copy())    
-    print('Base survival Cox: {:.5}'.format(es_cox.get_base_surv(1826)))
-    print('Base survival CML: {:.5}'.format(es_cml.get_base_surv(1826)))
-    return
-    
-    # evaluate
+    print('Evaluate on each fold...')
     for fold in range(hp.num_folds):
-        print('Fold: {}'.format(fold))
-        
-        es_cox = EvalSurv(df_cox.loc[data['fold'] == fold].copy())
-        es_cml = EvalSurv(df_cml.loc[data['fold'] == fold].copy())
+        for swap in range(2):
+            print('Fold: {} Swap: {}'.format(fold, swap))
+            
+            idx = (data['fold'][:, fold] == swap)
+            df_fold = df[idx].reset_index(drop=True)
+    
+            df_cox = df_fold.copy()
+            df_cml = df_fold.copy()
 
-        r2_vec_cox[fold] = es_cox.R_squared_D()
-        d_index_vec_cox[fold], _ = es_cox.D_index()
-        concordance_vec_cox[fold] = es_cox.concordance_index()
-        ibs_vec_cox[fold] = es_cox.integrated_brier_score()
-        auc_vec_cox[fold] = es_cox.auc(1826)
-        
-        r2_vec_cml[fold] = es_cml.R_squared_D()
-        d_index_vec_cml[fold], _ = es_cml.D_index()
-        concordance_vec_cml[fold] = es_cml.concordance_index()
-        ibs_vec_cml[fold] = es_cml.integrated_brier_score()
-        auc_vec_cml[fold] = es_cml.auc(1826)
+            # load log partial hazards
+            df_cox['LPH'] = feather.read_dataframe(hp.results_dir + 'df_cox_' + hp.gender + '_fold_' + str(fold) + '_' + str(swap) + '.feather')['LPH']
+            df_cml['LPH'] = feather.read_dataframe(hp.results_dir + 'df_cml_' + hp.gender + '_fold_' + str(fold) + '_' + str(swap) + '.feather')['LPH']
+    
+            ################################################################################################
+            
+            # baseline survival
+            # es_cox = EvalSurv(df_cox.copy())
+            # es_cml = EvalSurv(df_cml.copy())    
+            # print('Base survival Cox: {:.5}'.format(es_cox.get_base_surv(1826)))
+            # print('Base survival CML: {:.5}'.format(es_cml.get_base_surv(1826)))
+            # return
+                            
+            es_cox = EvalSurv(df_cox.copy())
+            es_cml = EvalSurv(df_cml.copy())
+
+            r2_vec_cox[fold, swap] = es_cox.R_squared_D()
+            d_index_vec_cox[fold, swap], _ = es_cox.D_index()
+            concordance_vec_cox[fold, swap] = es_cox.concordance_index()
+            ibs_vec_cox[fold, swap] = es_cox.integrated_brier_score()
+            auc_vec_cox[fold, swap] = es_cox.auc(1826)
+            
+            r2_vec_cml[fold, swap] = es_cml.R_squared_D()
+            d_index_vec_cml[fold, swap], _ = es_cml.D_index()
+            concordance_vec_cml[fold, swap] = es_cml.concordance_index()
+            ibs_vec_cml[fold, swap] = es_cml.integrated_brier_score()
+            auc_vec_cml[fold, swap] = es_cml.auc(1826)
+
+    print('Save...')
+    np.savez(hp.results_dir + 'eval_vecs_' + hp.gender + '.npz', 
+             r2_vec_cox=r2_vec_cox, d_index_vec_cox=d_index_vec_cox, concordance_vec_cox=concordance_vec_cox, ibs_vec_cox=ibs_vec_cox, auc_vec_cox=auc_vec_cox, 
+             r2_vec_cml=r2_vec_cml, d_index_vec_cml=d_index_vec_cml, concordance_vec_cml=concordance_vec_cml, ibs_vec_cml=ibs_vec_cml, auc_vec_cml=auc_vec_cml)
+
+    r2_vec_cox=np.reshape(r2_vec_cox,-1)
+    d_index_vec_cox=np.reshape(d_index_vec_cox,-1)
+    concordance_vec_cox=np.reshape(concordance_vec_cox,-1)
+    ibs_vec_cox=np.reshape(ibs_vec_cox,-1)
+    auc_vec_cox=np.reshape(auc_vec_cox,-1)
+    r2_vec_cml=np.reshape(r2_vec_cml,-1)
+    d_index_vec_cml=np.reshape(d_index_vec_cml,-1)
+    concordance_vec_cml=np.reshape(concordance_vec_cml,-1)
+    ibs_vec_cml=np.reshape(ibs_vec_cml,-1)
+    auc_vec_cml=np.reshape(auc_vec_cml,-1)
 
     r2_mean, (r2_lci, r2_uci) = r2_vec_cox.mean(), sms.DescrStatsW(r2_vec_cox).tconfint_mean()
     print('R-squared(D) Cox (95% CI): {:.3} ({:.3}, {:.3})'.format(r2_mean, r2_lci, r2_uci))
